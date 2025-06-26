934769de4b06496cd431c446e88fed3d7a108254
IMadeSumertodano <semertodhana22@gmail.com>
2025-06-26 19:36:47 +0800

    tambah route fungsi getPlaylistActivitiesHandler

--- src/api/playlists/routes.js
+++ src/api/playlists/routes.js
@@ -47,6 +47,14 @@ const routes = (handler) => [
       auth: "musicsapp_jwt",
     },
   },
+  {
+    method: "GET",
+    path: "/playlists/{id}/activities",
+    handler: (request, h) => handler.getPlaylistActivitiesHandler(request, h),
+    options: {
+      auth: "musicsapp_jwt",
+    },
+  },
 ];
 
 module.exports = routes;
