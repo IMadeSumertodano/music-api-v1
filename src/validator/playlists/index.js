2f3b473728458affca6e52db19cc206f842c0a8a
IMadeSumertodano <semertodhana22@gmail.com>
2025-06-26 14:17:19 +0800

    membuat validateSongToPlaylistPayload

--- src/validator/playlists/index.js
+++ src/validator/playlists/index.js
@@ -1,11 +1,21 @@
 const InvariantError = require("../../exceptions/InvariantError");
-const { PlaylistPayloadSchema } = require("./schema");
+const {
+  PlaylistPayloadSchema,
+  SongsToPlaylistPayloadSchema,
+} = require("./schema");
 
 const PlaylistsValidator = {
   validatePlaylistPayload: (payload) => {
     const validationResult = PlaylistPayloadSchema.validate(payload);
     if (validationResult.error) {
       throw new InvariantError(validationResult.error.message);
+    }
+  },
+
+  validateSongToPlaylistPayload: (payload) => {
+    const validateResult = SongsToPlaylistPayloadSchema.validate(payload);
+    if (validateResult.error) {
+      throw new InvariantError(validateResult.error.message);
     }
   },
 };
