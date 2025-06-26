c66bdc040f8a07d4aa93bfc06c4bf757fecd5c5c
IMadeSumertodano <semertodhana22@gmail.com>
2025-06-26 13:48:38 +0800

    ubah notnull jadi true

--- src/validator/playlists/schema.js
+++ src/validator/playlists/schema.js
@@ -2,7 +2,7 @@ const Joi = require("joi");
 
 const PlaylistPayloadSchema = Joi.object({
   name: Joi.string().required(),
-  owner: Joi.string(),
+  owner: Joi.string().required(),
 });
 
 module.exports = { PlaylistPayloadSchema };
