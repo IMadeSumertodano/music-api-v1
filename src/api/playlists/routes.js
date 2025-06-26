bf4863f4fadae6011ad42f0c16809664b0499dc0
IMadeSumertodano <semertodhana22@gmail.com>
2025-06-26 11:50:10 +0800

    Memproteksi Resource Playlists

--- src/api/playlists/routes.js
+++ src/api/playlists/routes.js
@@ -3,31 +3,49 @@ const routes = (handler) => [
     method: "POST",
     path: "/playlists",
     handler: (request, h) => handler.postPlaylistHandler(request, h),
+    options: {
+      auth: "musicsapp_jwt",
+    },
   },
   {
     method: "GET",
     path: "/playlists",
     handler: (request, h) => handler.getPlaylistHandler(request, h),
+    options: {
+      auth: "musicsapp_jwt",
+    },
   },
   {
     method: "DELETE",
     path: "/playlists/{id}",
     handler: (request, h) => handler.deletePlaylistHandler(request, h),
+    options: {
+      auth: "musicsapp_jwt",
+    },
   },
   {
     method: "POST",
     path: "/playlists/{id}/songs",
     handler: (request, h) => handler.postSongToPlaylistHandler(request, h),
+    options: {
+      auth: "musicsapp_jwt",
+    },
   },
   {
     method: "GET",
     path: "/playlists/{id}/songs",
     handler: (request, h) => handler.getSongInPlaylistHandler(request, h),
+    options: {
+      auth: "musicsapp_jwt",
+    },
   },
   {
     method: "DELETE",
     path: "/playlists/{id}/songs",
     handler: (request, h) => handler.deleteSongFromPlaylistHandler(request, h),
+    options: {
+      auth: "musicsapp_jwt",
+    },
   },
 ];
 
