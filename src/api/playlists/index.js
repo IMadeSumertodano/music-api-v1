0051f71ac84ded5156bb76977ad33b3ae6fec989
IMadeSumertodano <semertodhana22@gmail.com>
2025-06-27 09:22:49 +0800

    ubah register

--- src/api/playlists/index.js
+++ src/api/playlists/index.js
@@ -4,16 +4,8 @@ const routes = require("./routes");
 module.exports = {
   name: "playlists",
   version: "1.0.0",
-  register: async (
-    server,
-    { playlistsService, usersService, tokenManager, validator }
-  ) => {
-    const playlistsHandler = new PlaylistsHandler(
-      playlistsService,
-      usersService,
-      tokenManager,
-      validator
-    );
+  register: async (server, { service, validator }) => {
+    const playlistsHandler = new PlaylistsHandler(service, validator);
 
     server.route(routes(playlistsHandler));
   },
