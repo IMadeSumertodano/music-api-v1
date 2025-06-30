f3afa2ae0be30ef0e5db78f86a2caf839b410f81
IMadeSumertodano <semertodhana22@gmail.com>
2025-06-30 22:55:44 +0800

    perbaiki typo

--- src/api/uploads/routes.js
+++ src/api/uploads/routes.js
@@ -16,7 +16,7 @@ const routes = (handler) => [
   },
   {
     method: "GET",
-    path: "/upload/{param*}",
+    path: "/albums/covers/{param*}",
     handler: {
       directory: {
         path: path.resolve(__dirname, "file"),
