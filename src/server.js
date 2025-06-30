60feb2913aa62adc54f93a91b8c6613a4d08b670
IMadeSumertodano <semertodhana22@gmail.com>
2025-06-30 22:43:12 +0800

    Melayani Berkas Statis di Hapi

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 const Hapi = require("@hapi/hapi");
 const Jwt = require("@hapi/jwt");
+const Inert = require("@hapi/inert");
 const path = require("path");
 
 const ClientError = require("./exceptions/ClientError");
@@ -76,6 +77,9 @@ const init = async () => {
   await server.register([
     {
       plugin: Jwt,
+    },
+    {
+      plugin: Inert,
     },
   ]);
 
